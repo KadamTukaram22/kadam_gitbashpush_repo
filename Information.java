f4ff7ea6628ce38e6dbfa3803ea138245c4cd372
KadamTukaram22 <117625067+KadamTukaram22@users.noreply.github.com>
2023-09-03 10:52:58 +0530

    added

---
+++ Information.java
@@ -0,0 +1,14 @@
+package com.biodata;
+import com.education.*;
+
+public class Information{
+public static void main (String args [])
+{
+System.out.println("kadam tukaram pandurang");
+Username kada= new Username();
+System.out.println(kada.uid);
+kada.m1();
+
+}
+
+}
